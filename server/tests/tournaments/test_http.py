ed8520d7c960cfa72c0db663c8bc82205615f2fe
Odusseus55 <odusseus.development@gmail.com>
2022-01-24 15:54:13 +0100

    project setup, sign up and login feature added with access and refresh tokens

---
+++ server/tests/tournaments/test_http.py
@@ -0,0 +1,55 @@
+import base64
+import json
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+PASSWORD = 'pAssw0rd!'
+
+def create_user(username='user@example.com', password=PASSWORD):
+    return get_user_model().objects.create_user(
+        username=username,
+        first_name='Test',
+        last_name='User',
+        password=password
+    )
+
+
+class AuthenticationTest(APITestCase):
+    def test_user_can_sign_up(self):
+        response = self.client.post(reverse('sign_up'), data={
+            'username': 'user@example.com',
+            'first_name': 'Test',
+            'last_name': 'User',
+            'password1': PASSWORD,
+            'password2': PASSWORD,
+        })
+        user = get_user_model().objects.last()
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data['id'] == user.id
+        assert response.data['username'] == user.username
+        assert response.data['first_name'] == user.first_name
+        assert response.data['last_name'] == user.last_name
+
+    def test_user_can_log_in(self):
+        user = create_user()
+        response = self.client.post(reverse('log_in'), data={
+            'username': user.username,
+            'password': PASSWORD,
+        })
+
+        # Parse payload data from access token.
+        access = response.data['access']
+        header, payload, signature = access.split('.')
+        decoded_payload = base64.b64decode(f'{payload}==')
+        payload_data = json.loads(decoded_payload)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['refresh'] != None
+        assert payload_data['id'] == user.id
+        assert payload_data['username'] == user.username
+        assert payload_data['first_name'] == user.first_name
+        assert payload_data['last_name'] == user.last_name
+
+    
---
+++ server/tournaments/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .serializers import UserSerializer, LogInSerializer
+
+
+# Create your views here.
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView): 
+    serializer_class = LogInSerializer
